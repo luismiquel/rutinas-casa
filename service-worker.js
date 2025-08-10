e4329e5450d932177a3cc9354219c88ba2950365
Luis Miguel <luismiguelgarciadelasmorenas@gmail.com>
2025-08-10 19:37:50 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,35 @@
+const CACHE_NAME = "entrenador-cache-v2";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "https://actions.google.com/sounds/v1/alarms/beep_short.ogg"
+];
+
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("entrenador-cache-v1").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
