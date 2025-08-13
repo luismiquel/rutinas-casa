88ca9bf0c26d4ccb32df57db547596c2ace47b87
Luis Miguel <luismiguelgarciadelasmorenas@gmail.com>
2025-08-13 20:33:53 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("entrenador-cache-v1").then(cache => {
+    caches.open("entrenador-cache-v2").then(cache => {
       return cache.addAll([
         "./",
         "./index.html",
@@ -8,12 +8,29 @@ self.addEventListener("install", event => {
       ]);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(k => k !== "entrenador-cache-v2").map(k => caches.delete(k))
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+      return response || fetch(event.request).catch(() => {
+        // Si es un archivo HTML y no hay conexión, devuelve el index.html
+        if (event.request.headers.get("accept").includes("text/html")) {
+          return caches.match("./index.html");
+        }
+      });
     })
   );
 });
